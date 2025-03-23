b01e0848a3baa43df48967ae2cc0c97cc9df9ab3
KJFeng23 <125720043+KJFeng23@users.noreply.github.com>
2025-03-24 00:16:27 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php 
-echo "Welcome";
+session_start();
 ?>
+
+<DOCTYPE html>
+<html>
+
+<header>
+
+<meta charset="UTF-8">
+<title>Home Page</title>
+
+</header>
+
+<body>
+<h1> Welcome to Culinary Website</h1>
+
+<nav name="navbar">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+
+</body>
+
+
+</html>
